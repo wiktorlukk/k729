7c0f8ffbde8a7ef6f3987f3f8e3bb340f0f7661a
Wiktor Liszka <wiktor.liszka@gmail.com>
2025-07-12 18:39:51 +0200

    gunwo

---
+++ data.js
@@ -0,0 +1,329 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-kuchnia",
+      "name": "kuchnia",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.6919008055047087,
+        "pitch": 0.24424905675416042,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.446349445879153,
+          "pitch": 0.17650865276624828,
+          "rotation": 0,
+          "target": "1-azienka"
+        },
+        {
+          "yaw": -2.5393038222221413,
+          "pitch": 0.3366532736695653,
+          "rotation": 0,
+          "target": "2-pokj"
+        },
+        {
+          "yaw": -1.576756541757236,
+          "pitch": 0.409724914594193,
+          "rotation": 0,
+          "target": "3-salon1"
+        },
+        {
+          "yaw": -1.5746195628156823,
+          "pitch": 0.23930647133194505,
+          "rotation": 0,
+          "target": "5-salon3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-azienka",
+      "name": "łazienka",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.535214019594445,
+        "pitch": 0.32761711301220586,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4100946751183,
+          "pitch": 0.29019451251761197,
+          "rotation": 0,
+          "target": "0-kuchnia"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-pokj",
+      "name": "pokój",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.088099863151122,
+        "pitch": 0.4269821728657366,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8747254370084843,
+          "pitch": 0.39253562844322154,
+          "rotation": 0,
+          "target": "0-kuchnia"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-salon1",
+      "name": "salon1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.08983419325914355,
+        "pitch": 0.18204133993089044,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.06471420447362775,
+          "pitch": 0.3047297950198402,
+          "rotation": 0,
+          "target": "5-salon3"
+        },
+        {
+          "yaw": 0.007505848972144236,
+          "pitch": 0.9240949087686765,
+          "rotation": 0,
+          "target": "4-salon2"
+        },
+        {
+          "yaw": -2.7899111536789842,
+          "pitch": 0.057682482048971195,
+          "rotation": 0,
+          "target": "1-azienka"
+        },
+        {
+          "yaw": -2.4193482038547494,
+          "pitch": 0.07048842893495433,
+          "rotation": 0,
+          "target": "2-pokj"
+        },
+        {
+          "yaw": 2.928792089244154,
+          "pitch": 0.08471026735243647,
+          "rotation": 0,
+          "target": "0-kuchnia"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-salon2",
+      "name": "salon2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 1.0718909308137903,
+        "pitch": 0.20673014120894706,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4149886324560264,
+          "pitch": 0.5006036379959795,
+          "rotation": 0,
+          "target": "5-salon3"
+        },
+        {
+          "yaw": 1.6863341185891842,
+          "pitch": 0.7999935535269351,
+          "rotation": 0,
+          "target": "3-salon1"
+        },
+        {
+          "yaw": 2.326867440176537,
+          "pitch": 0.017900649733277874,
+          "rotation": 0,
+          "target": "2-pokj"
+        },
+        {
+          "yaw": 1.3721835777874176,
+          "pitch": 0.09785171936944437,
+          "rotation": 0,
+          "target": "0-kuchnia"
+        },
+        {
+          "yaw": 1.981106836251282,
+          "pitch": 0.08025896007794664,
+          "rotation": 0,
+          "target": "1-azienka"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-salon3",
+      "name": "salon3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.7889398385547981,
+        "pitch": 0.3587408021076577,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5293611074683593,
+          "pitch": 0.19504498993464559,
+          "rotation": 0,
+          "target": "2-pokj"
+        },
+        {
+          "yaw": 0.032412149373019616,
+          "pitch": 0.08623806976361692,
+          "rotation": 0,
+          "target": "0-kuchnia"
+        },
+        {
+          "yaw": 0.20697576575480348,
+          "pitch": 0.7833866280187465,
+          "rotation": 0,
+          "target": "4-salon2"
+        },
+        {
+          "yaw": 0.14967400587551616,
+          "pitch": 0.5371006936214933,
+          "rotation": 0,
+          "target": "3-salon1"
+        },
+        {
+          "yaw": 0.3376256306211065,
+          "pitch": 0.09982246233568404,
+          "rotation": 0,
+          "target": "1-azienka"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
